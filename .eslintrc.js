425da6b6233ea891b8e8b74a91bce461f8cf7dd2
nqminh2417 <quangminh2417@gmail.com>
2025-11-18 20:10:17 +0700

    Refactor App component and update ESLint configuration
    - Simplified App component by removing unused imports and code.
    - Added ESLint rule to disable inline styles warning.
    - Updated package.json and yarn.lock to reflect new version of react-native-safe-area-context.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // Tắt hẳn rule inline styles
+    'react-native/no-inline-styles': 'off',
+  },
 };
--- App.tsx
+++ App.tsx
@@ -1,45 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
